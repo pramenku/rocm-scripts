ee615ffcaf33e10dfac51e0aee55401d08b38533
pramenku <7664080+pramenku@users.noreply.github.com>
2019-10-19 23:03:12 +0530

    Update pytorch-run.sh

--- pytorch-run.sh
+++ pytorch-run.sh
@@ -2,8 +2,6 @@ current=`pwd`
 LOGDIR=$current
 export PYTORCH_TEST_WITH_ROCM=1 
 
-update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
-
 echo "========================= py-autograd====================="
 python /root/pytorch/test/test_autograd.py -v 2>&1 | tee $LOGDIR/py-autograd.log
 echo "========================= py-cuda====================="
