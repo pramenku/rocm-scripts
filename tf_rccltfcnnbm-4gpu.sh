38e20acf21d4935f93a108156b3eff0676ea7ed5
pramenku <7664080+pramenku@users.noreply.github.com>
2021-01-18 19:11:12 +0530

    Update tf_rccltfcnnbm-4gpu.sh

--- tf_rccltfcnnbm-4gpu.sh
+++ tf_rccltfcnnbm-4gpu.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # run_soumith_benchmarks is not applicable for TF1.8 and higher. refer https://github.com/soumith/convnet-benchmarks/issues/138
 #This script is based on python3 . When TF was build with python3, use python3 . When TF was built with python2, replace python3 to python2
-export HIP_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+#export HIP_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 #export HSA_ENABLE_SDMA=0
 
 cwd=`pwd`
 BASEDIR=/root
 LOGDIR=/dockerx
+
+num_gpus=$(lspci|grep 'controller'|grep 'AMD/ATI'|wc -l)
+echo $num_gpus
 
 mkdir -p /dockerx/tf-rccllogs
 
@@ -24,7 +27,7 @@ run_tf_cnn_benchmarks()
 #     MODELS="alexnet"
        export TF_ROCM_FUSION_ENABLE=1
         MODELS="alexnet googlenet inception3 inception4 resnet50 resnet152_v2 vgg11 vgg16 vgg19 resnet101"
-	NGPUS=8
+	NGPUS=$num_gpus
 	ITERATIONS=100
 	#BATCH_SIZE="1 2 4 8 16 32 64"
 	BATCH_SIZE="64"
@@ -47,8 +50,9 @@ run_tf_cnn_benchmarks_128()
     echo "=======================tf_cnn_benchmarks_BS128==============="
         cd $BENCHDIR
     export TF_ROCM_FUSION_ENABLE=1
-    MODELS="alexnet googlenet inception3 lenet overfeat resnet50 trivial vgg11 vgg16 vgg19"
-        NGPUS=8
+    #MODELS="alexnet googlenet inception3 lenet overfeat resnet50 trivial vgg11 vgg16 vgg19"
+    MODELS="resnet50 vgg19"
+        NGPUS=$num_gpus
         ITERATIONS=100
         BATCH_SIZE=128
 
@@ -70,8 +74,9 @@ run_tf_cnn_benchmarks_256()
     echo "=======================tf_cnn_benchmarks_BS256==============="
         cd $BENCHDIR
     export TF_ROCM_FUSION_ENABLE=1
-    MODELS="alexnet googlenet resnet50_v1.5"
-        NGPUS=8
+    #MODELS="alexnet googlenet resnet50_v1.5"
+    MODELS="resnet50_v1.5"
+        NGPUS=$num_gpus
         ITERATIONS=100
         BATCH_SIZE=256
 
@@ -134,9 +139,9 @@ run_tf_cnn_benchmarks_1024()
 } 
 
 
-run_tf_cnn_benchmarks
+#run_tf_cnn_benchmarks
 run_tf_cnn_benchmarks_128
 run_tf_cnn_benchmarks_256
-run_tf_cnn_benchmarks_512
-run_tf_cnn_benchmarks_1024
+#run_tf_cnn_benchmarks_512
+#run_tf_cnn_benchmarks_1024
 
